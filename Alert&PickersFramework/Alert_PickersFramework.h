d8cd9d70181f1fafba8439e9b7c476902b3786bc
PiotrMielcarzewicz <piotr.mielcarzewicz@appunite.com>
2018-04-04 09:14:30 +0200

    added shared framework

---
+++ Alert&PickersFramework/Alert_PickersFramework.h
@@ -0,0 +1,19 @@
+//
+//  Alert_PickersFramework.h
+//  Alert&PickersFramework
+//
+//  Created by Piotr Mielcarzewicz on 04/04/2018.
+//  Copyright © 2018 Supreme Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Alert_PickersFramework.
+FOUNDATION_EXPORT double Alert_PickersFrameworkVersionNumber;
+
+//! Project version string for Alert_PickersFramework.
+FOUNDATION_EXPORT const unsigned char Alert_PickersFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alert_PickersFramework/PublicHeader.h>
+
+
---
+++ AlertsAndPickers/AlertsAndPickers.h
@@ -0,0 +1,19 @@
+//
+//  AlertsAndPickers.h
+//  AlertsAndPickers
+//
+//  Created by Piotr Mielcarzewicz on 04/04/2018.
+//  Copyright © 2018 Supreme Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlertsAndPickers.
+FOUNDATION_EXPORT double AlertsAndPickersVersionNumber;
+
+//! Project version string for AlertsAndPickers.
+FOUNDATION_EXPORT const unsigned char AlertsAndPickersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertsAndPickers/PublicHeader.h>
+
+
